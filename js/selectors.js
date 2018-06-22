7769cd595250f77aa2ab616d7f78e9ade78b2847
rosyjackson <rosyjackson2004@gmail.com>
2018-06-22 18:29:23 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,22 @@
 'use-strict';
 //add your code here
+
+function h1Selector(){
+  return $('h1')
+}
+
+function liInOlSelector(){
+  return $('ol li')
+}
+
+function linkSelector(){
+  return $('a','#box4','.box5')
+}
+
+function imageSelector(){
+  return $("[alt='cat sleeping']")
+}
+
+function checkboxInputSelector(){
+  return $('input[type=checkbox]')
+}
